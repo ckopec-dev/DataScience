6eba9c2c949dca18bb144b35a13672144d72a146
Chris Kopec <c_kopec@hotmail.com>
2023-08-06 19:01:05 -0400

    Experiment1

--- Experiment1.py
+++ Experiment1.py
@@ -1,1 +1,22 @@
 
+#
+#
+# Euler's identity: e^(pi*i) = -1
+#
+#
+#
+
+import math, cmath
+
+z = complex(0, 1)
+exp = math.pi * z
+z = cmath.exp(exp)
+
+print ("The real part of complex number is : ", z.real)
+print ("The imaginary part of complex number is : ", z.imag)
+
+
+# Evaluate iteratively
+
+for n in range(1, 4):
+    print("TODO")    
