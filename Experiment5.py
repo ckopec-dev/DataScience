e95c40e7c3872c99f95aa2e49bb224c7572569a0
Chris Kopec <c_kopec@hotmail.com>
2023-08-06 19:54:35 -0400

    Experiment5

---
+++ Experiment5.py
@@ -0,0 +1,16 @@
+#
+#
+#
+# SymPy test
+#
+# https://stackoverflow.com/questions/567222/simple-prime-number-generator-in-python
+# https://www.sympy.org/en/index.html
+#
+# To install: $ pip3 install sympy
+#
+#
+#
+
+import sympy
+
+print(sympy.isprime(5))
