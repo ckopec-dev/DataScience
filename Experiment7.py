dc5a63f6056db627aa6b197a5844ae5af2644a5f
Chris Kopec <c_kopec@hotmail.com>
2023-08-07 19:45:10 -0400

    Exp 7 update

--- Experiment7.py
+++ Experiment7.py
@@ -9,5 +9,8 @@
 from Helper import *
 from tabulate import tabulate
 
-print(tabulate([[1000, PrimesLessThanN(1000), NoverN(1000)]],
+print(tabulate([[100, PrimesLessThanN(100), NoverN(100)],
+                [1000, PrimesLessThanN(1000), NoverN(1000)],
+                [1000000, PrimesLessThanN(1000000), NoverN(1000000)]
+               ],
                headers=['N', 'PrimesLessThanN', 'NoverN']))
