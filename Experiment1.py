0a5c90616d657effd1f9eae82c382cc112d99b30
Chris Kopec <c_kopec@hotmail.com>
2023-08-18 17:11:54 -0400

    Experiment1 iteratively

--- Experiment1.py
+++ Experiment1.py
@@ -2,6 +2,8 @@
 #
 #
 # Euler's identity: e^(pi*i) = -1
+# 
+# Complex power of e: e^z = 1 + z + z^2/1x2 + z^3/1x2x3 ... 
 #
 #
 #
@@ -14,8 +16,14 @@ z = cmath.exp(exp)
 
 print ("The real part of complex number is : ", z.real)
 print ("The imaginary part of complex number is : ", z.imag)
+print (z)
 
 # Evaluate iteratively
+print("Evaluating iteratively...")
+z = complex(0, math.pi)
+e = complex(1, 0)
+for n in range(1, 100):
+    print(n)
+    e = e + (z ** n / math.factorial(n))
+    print (e)
 
-for n in range(1, 4):
-    print("TODO")    
