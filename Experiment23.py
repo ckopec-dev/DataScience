9804ddcb2e400a521eedd4a945714b3e1b0c66b5
Chris Kopec <c_kopec@hotmail.com>
2023-08-26 14:10:50 -0400

    Experiment23

---
+++ Experiment23.py
@@ -0,0 +1,10 @@
+
+# Sage demo from python script
+# https://doc.sagemath.org/html/en/tutorial/index.html
+
+# To run from console, use:
+# $ sage -python Experiment23.py
+
+from sage.all import *
+
+print(factor(-2007))
