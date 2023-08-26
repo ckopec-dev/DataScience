16740927e1ad463796b66240656e811df0260e4f
Chris Kopec <c_kopec@hotmail.com>
2023-08-26 14:03:39 -0400

    Experiment22

---
+++ Experiment22.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+#
+#
+#
+# Shebang hello world
+#
+# To make executable:
+# $ chmod +x Experiment22.py
+#
+# Execute with:
+# $ ./Experiment22.py
+#
+#
+#
+
+print("Hello world!")
