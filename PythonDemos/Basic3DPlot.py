adf5e942fe4c18d43102c1fd7875681578e7a4f3
Chris Kopec <c_kopec@hotmail.com>
2023-10-12 19:10:33 -0400

    Added scatter points to Basic3DPlot

--- PythonDemos/Basic3DPlot.py
+++ PythonDemos/Basic3DPlot.py
@@ -6,12 +6,17 @@ fig = plt.figure()
 
 ax = plt.axes(projection='3d')
 
-#3d line plot
+# 3d line
 zline = np.linspace(0, 10, 100)
 xline = np.sin(zline)
 yline = np.cos(zline)
-
 ax.plot3D(xline, yline, zline, 'red')
+
+# 3d scatter points
+zdata = 15 * np.random.random(100)
+xdata = np.sin(zdata) + 0.1 * np.random.randn(100)
+ydata = np.cos(zdata) + 0.1 * np.random.randn(100)
+ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='Greens');
 
 plt.savefig("Basic3DPlot.png")
 plt.show()
